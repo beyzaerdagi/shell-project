82b42d32ed704361ec485ca8a91fd087cdb00daa
Beyza <69843122+beyzaerdagi@users.noreply.github.com>
2022-11-24 19:51:32 +0300

    Update execx.c

--- execx.c
+++ execx.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-
+//Clear terminal
 #define clear() printf("\033[H\033[J") 
 
 int main(int argc, char *argv[]) {
@@ -14,12 +14,17 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[3], "clear") == 0){
         clear();
     }else if (strcmp(argv[3], "ls") == 0){
+        //Prints the contents of the folder up to the entered times
         for (int i = 0; i < atoi(argv[2]);i++){
             system("/bin/ls");
         }
     }else if (strcmp(argv[3], "cat") == 0){
         for (int i = 0; i < atoi(argv[2]);i++){
-            printf("cat:%s\n", argv[4]);
+            printf("Cat: ");
+            for (int j = 4; argv[j] != '\0'; j++){
+                printf("%s ", argv[j]);
+            }
+            printf("\n");
         }
     }else if (strcmp(argv[3], "bash") == 0){
         for (int i = 0; i < atoi(argv[2]);i++){
@@ -32,9 +37,11 @@ int main(int argc, char *argv[]) {
             f = fork();
             if (f == 0){
                 status = execve("writef", argv, NULL);
+                // When a child process finishes it exits and other child processes run
                 exit(0);
                 perror("exec failed");
             }else{
+                // Waits until the child process has finished
                 wait(&status);
             }
         }
