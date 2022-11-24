ca9b270641bc2442f8694f9ccc76403a40d0d369
Beyza <69843122+beyzaerdagi@users.noreply.github.com>
2022-11-24 14:41:19 +0300

    Update writef.c

--- writef.c
+++ writef.c
@@ -7,56 +7,45 @@
 
 int main(int argc, char *argv[]) {
 
-    size_t n = 10;
-    char *buf = malloc(sizeof(char) * n);
-    printf("Enter input: ");
-    getline(&buf, &n, stdin);
+        size_t n = 10;
+        char *buf = malloc(sizeof(char) * n);
+        printf("Enter input: ");
+        getline(&buf, &n, stdin);
 
-    char snum[10];
-    char snum2[10];
-    sprintf(snum, "%d", getpid());
-    sprintf(snum2, "%d", getppid());
-    time_t rawtime;
-    struct tm * timeinfo;
+        char snum[10];
+        char snum2[10];
+        sprintf(snum, "%d", getpid());
+        sprintf(snum2, "%d", getppid());
+        time_t rawtime;
+        struct tm * timeinfo;
 
-    time (&rawtime);
-    timeinfo = localtime (&rawtime);
+        time (&rawtime);
+        timeinfo = localtime (&rawtime);
 
-    FILE *fptr;
-    char ch;
-    char *concatChar[100];
-    char *content[6];
-    content[0] = "System time:";
-    content[1] = asctime (timeinfo);
-    content[2] = "Pid:";
-    content[3] = snum;
-    content[4] = " PPid:";
-    content[5] = snum2;
+        char *systemTime = asctime(timeinfo);
+        systemTime[strlen(systemTime) - 1] = 0;
 
-    for (int i = 0; i < size ;i++){
-        strcat(concatChar, content[i]);
-    }
-    strcat(concatChar, "\n");
+        FILE *fptr;
+        char ch;
+        
 
-    fptr = fopen(argv[argc - 1], "a");
+        fptr = fopen(argv[argc - 1], "a");
 
-    if (fptr != NULL) {
-        fprintf(fptr, "%s\n", buf);
-        fprintf(fptr, "%s\n", concatChar);
+        if (fptr != NULL) {
+            fprintf(fptr, "%s", buf);
+            fprintf(fptr, "System time: %s, Pid: %s, PPid: %s\n\n", systemTime, snum, snum2);
+            fclose(fptr);
+        }else{
+            fprintf(fptr, "%s", buf);
+            fprintf(fptr, "System time: %s, Pid: %s, PPid: %s\n\n", systemTime, snum, snum2);
+            fclose(fptr);
+        }
+        
+        fptr = fopen(argv[argc - 1], "r");
+    
+        while ((ch = fgetc(fptr) )!= EOF) {
+            printf ("%c", ch);
+        }
         fclose(fptr);
-    }else{
-        fprintf(fptr, "%s\n", buf);
-        fprintf(fptr, "%s\n", concatChar);
-        fclose(fptr);
-    }
-
-    fptr = fopen(argv[argc - 1], "r");
-   
-    while ((ch = fgetc(fptr) )!= EOF) {
-        printf ("%c", ch);
-    }
-    fclose(fptr);
-    free(concatChar);
-    free(content);
-    return 0;
+        return 0;
 }
