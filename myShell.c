d4d4172721d8e5b67007338a59244ef39e43d0cd
Beyza <69843122+beyzaerdagi@users.noreply.github.com>
2022-11-24 19:52:01 +0300

    Update myShell.c

--- myShell.c
+++ myShell.c
@@ -11,7 +11,8 @@
 
 char *commandArr[] = {"exit","clear","ls","bash","cat: cat input","writef: writef -f file_name","execx: execx -t times program"}; 
 
-void helpCommand(){ //shelldeki help komutunu ifade ediyor.
+// Shows available commands in the shell
+void helpCommand(){
 
   printf("Commands: \n");
 
@@ -23,30 +24,40 @@ void helpCommand(){ //shelldeki help komutunu ifade ediyor.
 void execute(char **args){
 
     int f, status;
-
-    if (strcmp("exit", args[0]) == 0){ // if command exit
-        exit(0); // terminate myshell program
-    }else if (strcmp("clear", args[0]) == 0){ // if command is clear
-        clear(); // clear shell
-    }else if(strcmp("cat", args[0]) == 0){  // Otherwise
-        printf("cat:%s\n", args[1]);
+    // if command exit
+    if (strcmp("exit", args[0]) == 0){ 
+        exit(0);
+    }else if (strcmp("clear", args[0]) == 0){ 
+        //clear shell
+        clear();
+    }else if(strcmp("cat", args[0]) == 0){
+        // print the input
+        printf("Cat: ");
+        for (int i = 1; args[i] != '\0'; i++){
+            printf("%s ", args[i]);
+        }
     }else if (strcmp("bash", args[0]) == 0){
+        // goes to system bash
         system("/bin/bash");
     }else if(strcmp("ls", args[0]) == 0){
+        // fetches the contents of the folder
         system("/bin/ls");
     }else if(strcmp("help",args[0]) == 0){
         helpCommand();
     }else if(strcmp("writef",args[0]) == 0){
         f = fork();
         if (f == 0){
+            // calls the writef function
             status = execve("writef", args, NULL);
             perror("exec failed");
         }else{
+            // Waits until the child process has finished
             wait(&status);
         }
     }else if(strcmp("execx",args[0]) == 0){
         f = fork();
         if (f == 0){
+            // calls the execx function
             status = execve("execx", args, NULL);
             perror("exec failed");
         }else{
@@ -57,9 +68,8 @@ void execute(char **args){
     }
 }
 
-
-char **splitLine(char *command)  //split line with token https://brennan.io/2015/01/16/write-a-shell-in-c/
-{
+//split line with token https://brennan.io/2015/01/16/write-a-shell-in-c/
+char **splitLine(char *command) {
 
   int bufsize = BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char *));
@@ -98,9 +108,11 @@ int main(){
 
         printf("\nmyshell>>");
         scanf("%[^\n]%*c", command);
+        // separated into input lines from the user
         param = splitLine(command);
         execute(param);
     }
-    free(command); //malloc ile kullandığımız kaynağı geri iade ediyoruz 
+    // Returns array retrieved with malloc
+    free(command);  
     free(param);
 }
