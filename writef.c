13076dbc9dfaffb34a30f9c8f733a23e2100804c
Beyza <69843122+beyzaerdagi@users.noreply.github.com>
2022-11-24 19:52:23 +0300

    Update writef.c

--- writef.c
+++ writef.c
@@ -3,49 +3,51 @@
 #include <time.h>
 #include <string.h>
 
-#define size sizeof(content) / sizeof(char *)
-
 int main(int argc, char *argv[]) {
 
-        size_t n = 10;
-        char *buf = malloc(sizeof(char) * n);
-        printf("Enter input: ");
-        getline(&buf, &n, stdin);
+    FILE *fptr;
+    char ch;
+    size_t n = 10;
+    char *buf = malloc(sizeof(char) * n);
+    printf("Enter input: ");
+    getline(&buf, &n, stdin);
 
-        char snum[10];
-        char snum2[10];
-        sprintf(snum, "%d", getpid());
-        sprintf(snum2, "%d", getppid());
-        time_t rawtime;
-        struct tm * timeinfo;
+    char pid[10];
+    char ppid[10];
+    // Converts pid values ​​to string
+    sprintf(pid, "%d", getpid());
+    sprintf(ppid, "%d", getppid());
 
-        time (&rawtime);
-        timeinfo = localtime (&rawtime);
+    time_t rawtime;
+    struct tm * timeinfo;
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+    //Deletes the \n at the end of system time
+    char *systemTime = asctime(timeinfo);
+    systemTime[strlen(systemTime) - 1] = 0;
 
-        char *systemTime = asctime(timeinfo);
-        systemTime[strlen(systemTime) - 1] = 0;
+    //Opens the file as appended to the end
+    fptr = fopen(argv[argc - 1], "a");
 
-        FILE *fptr;
-        char ch;
-        
-
-        fptr = fopen(argv[argc - 1], "a");
-
-        if (fptr != NULL) {
-            fprintf(fptr, "%s", buf);
-            fprintf(fptr, "System time: %s, Pid: %s, PPid: %s\n\n", systemTime, snum, snum2);
-            fclose(fptr);
-        }else{
-            fprintf(fptr, "%s", buf);
-            fprintf(fptr, "System time: %s, Pid: %s, PPid: %s\n\n", systemTime, snum, snum2);
-            fclose(fptr);
-        }
-        
-        fptr = fopen(argv[argc - 1], "r");
-    
-        while ((ch = fgetc(fptr) )!= EOF) {
-            printf ("%c", ch);
-        }
+    if (fptr != NULL) {
+        // If the file does not exist, it creates the file and writes it to the file with the fprintf function
+        fprintf(fptr, "%s", buf);
+        fprintf(fptr, "System time: %s, Pid: %s, PPid: %s\n\n", systemTime, pid, ppid);
         fclose(fptr);
-        return 0;
+    }else{
+        // If the file exists, append it to the end of the file
+        fprintf(fptr, "%s", buf);
+        fprintf(fptr, "System time: %s, Pid: %s, PPid: %s\n\n", systemTime, pid, ppid);
+        fclose(fptr);
+    }
+    // Opens the file in read mode
+    fptr = fopen(argv[argc - 1], "r");
+    
+    // Prints the file content until it runs out
+    while ((ch = fgetc(fptr) )!= EOF) {
+        printf ("%c", ch);
+    }
+    // Close file
+    fclose(fptr);
+    return 0;
 }
